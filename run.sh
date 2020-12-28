de08ed399d78df8a555cd5e0458a629e485e4e70
eknabevcc <emil.knabe@volvocars.com>
2020-12-28 15:03:26 +0100

    Updates for esmini 2.X
    - Ubuntu 18.04 (instead of 16.04)
    - Fetch latest esmini release (instead of building)
    - Remove overlay info text (option in run.sh)
    - Option to specify video format  (GIF is still default)
    - Create a .dat file for later replay using with esmini/replayer

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 Xvfb :1 -screen 0 ${RESOLUTION}x16 &> xvfb.log  &
 
-cd /root/esmini/build/EnvironmentSimulator/EgoSimulator/
-target=$(echo $SCENARIO | sed 's/\(.*\).xosc/\1\.gif/')
-timeout $LENGTH ./EgoSimulator --window 0 0 ${RESOLUTION//x/ } --osc "/scenarios/${SCENARIO}" --ext_control off &
-avconv -f x11grab -s $RESOLUTION -r $FRAMERATE -t $LENGTH -y -i :1.0 "/scenarios/${target}"
+cd ~/esmini
+
+# GIF animation format is default. Can be changed into anything supported by ffmpeg, e.g. VIDEO_FORMAT=mp4
+target=`sed "s/\(.*\).xosc/\1\.${VIDEO_FORMAT}/" <<< $(basename $SCENARIO)`
+
+# Video and .dat file will end up in the appointed scenarios folder
+timeout $LENGTH ./bin/esmini --window 0 0 ${RESOLUTION//x/ } --osc "/scenarios/${SCENARIO}" --record /scenarios/`sed -e 's|.xosc|.dat|g' <<< $(basename ${SCENARIO})` --disable_controllers --aa_mode 0 --info_text off & 
+ffmpeg -f x11grab -s $RESOLUTION -r $FRAMERATE -t $LENGTH -y -i :1.0 "/scenarios/${target}"
